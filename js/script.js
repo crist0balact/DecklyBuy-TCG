8d248f618bd89ebb82ab6a633af8c4bfbd8dbb02
Seba <sebastian.ignacio.b2002@gmail.com>
2025-09-11 23:38:56 -0300

    Pagina categorias pokemon, id para boton categorias pokemon para codigo js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btnPokemon = document.getElementById("btn-pokemon");
 
+  if (btnPokemon) {
+    btnPokemon.addEventListener("click", function () {
+      window.location.href = "categories-pokemon.html";
+    });
+  }
+});
